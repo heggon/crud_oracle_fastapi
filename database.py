d212cd6091f699a5d91997584e16fe7366ac920a
heggon <heggon_diego@hotmail.com>
2023-05-06 22:16:38 -0300

    Update database.py
    Including cx_Oracle and folder path

--- database.py
+++ database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine.url import URL
+import cx_Oracle
+
+cx_Oracle.init_oracle_client(lib_dir=r"C:\Oracle\instantclient")
 
 connect_url = "oracle+cx_oracle://system:admin@localhost:1521/xe"
 
